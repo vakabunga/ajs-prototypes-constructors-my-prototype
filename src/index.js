9f967eb1219dcabed2a8e808119413f097b2c4c1
Vakabunga <shesterov@gmail.com>
2020-05-23 23:28:03 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+
+import './js/character';
+
+// TODO: write your code in app.js
---
+++ src/js/character.js
@@ -0,0 +1,7 @@
+export default function Character(name, type) {
+  this.name = name;
+  this.type = type;
+  this.health = 100;
+  this.attack = 10;
+  this.defence = 40;
+}
